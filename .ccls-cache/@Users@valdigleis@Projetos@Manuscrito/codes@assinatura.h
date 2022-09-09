4101487032751c70bd6e68303efe560467811bf0
Valdigleis <valdigleis@gmail.com>
2022-09-09 08:24:00 -0300

    :white_check_mark: Limpado pasta vazia

--- .ccls-cache/@Users@valdigleis@Projetos@Manuscrito/codes@assinatura.h
+++
@@ -1,5 +0,0 @@
-/* Assinaturas */
-int reverse(int x);
-unsigned int strlen (char *x);
-int strcmp (char *x, char *y);
-char *strcpy (char *y, char *x);
--- .ccls-cache/@Users@valdigleis@Projetos@Manuscrito/codes@sqfunction.c
+++
@@ -1,7 +0,0 @@
-int sqroot(int x){
-  int y = 0;
-  while(y*y != x){
-    y = y + 1;
-  }
-  return x;
-}
